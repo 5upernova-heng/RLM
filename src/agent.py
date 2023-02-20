177f65c34c3e646ab74bb96f1d6d9c8e9c033e2e
Hengyu <76626546+5upernova-heng@users.noreply.github.com>
2023-02-20 21:17:34 +0800

    重构代码

--- src/agent.py
+++
@@ -1,9 +0,0 @@
-class agent:
-    def __init__(self, start_pos) -> None:
-        agent.pos = start_pos
-
-    def get_pos(self):
-        return agent.pos
-
-    def change_pos(self, new_pos):
-        agent.pos = new_pos
---
+++ src/mainwindow.py
@@ -0,0 +1,33 @@
+# the logic of ui
+
+import sys
+from ui_main_window import *
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs) -> None:
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.show()
+
+    def draw():
+        """def draw(self, maze(height, width, agent_pos, drawType, ...)):
+        通过迷宫数据画出迷宫
+        canvas.frameGeometry(), drawEvent(), ..."""
+
+    def start(self):
+        """开始算法, 并禁用 comboBox 和 start 按钮
+        setEnable(False)"""
+
+    def pause(self):
+        """暂停算法, 将按钮改成 resume
+        button.clicked.connect/disconnect(func)"""
+
+    def change_algorithm(self):
+        """根据 comboBox 里面的 Text, 改变当前使用的算法"""
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec()
