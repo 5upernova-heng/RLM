1bf23b9c83c0285e9aeab616172b09f1b199e953
Hengyu <76626546+5upernova-heng@users.noreply.github.com>
2023-02-25 23:25:20 +0800

    Update UI

--- src/main_window.py
+++ src/main_window.py
@@ -4,9 +4,10 @@
 
 import sys
 from main_window_ui import *
+from PyQt6.QtWidgets import *
 
 
-class MainWindow(QMainWindow, MainWindowUI):
+class MainWindow(QMainWindow, UiMainWindow):
     def __init__(self, *args, **kwargs) -> None:
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
@@ -20,10 +21,6 @@ class MainWindow(QMainWindow, MainWindowUI):
     def start(self):
         """开始算法, 并禁用 comboBox 和 start 按钮
         setEnable(False)"""
-
-    # def pause(self):
-    #     """暂停算法, 将按钮改成 resume
-    #     button.clicked.connect/disconnect(func)"""
 
     def change_algorithm(self):
         """根据 comboBox 里面的 Text, 改变当前使用的算法"""
