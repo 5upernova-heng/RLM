6dbb62b71a416ea282caa370f2557572dac93b6d
Hengyu <76626546+5upernova-heng@users.noreply.github.com>
2023-02-26 21:31:12 +0800

    Init maze_generator

---
+++ src/maze_generator/maze_generator.py
@@ -0,0 +1,16 @@
+"""
+The Abstract Maze Generator class.
+
+Attribute:
+    width: The width of the maze.
+    height: The height of the maze.
+"""
+
+
+class MazeGenerator:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def generate(self):
+        pass
