fcbd231cf87c6dbd4cb845a2d1afca2c50cff273
Hengyu <h3ngyu@gmail.com>
2023-03-31 08:34:30 +0800

    Optimize q_learning algorithm

--- src/q_learning.py
+++ src/q_learning.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import numpy as np
 from solver import Solver
+from typing import *
 
 
 class QLearning(Solver):
@@ -8,35 +8,38 @@ class QLearning(Solver):
         self,
         action_list,
         learning_rate: float = 1,
-        reward_decay: float = 0.9,
-        epsilon: float = 0.9,
+        reward_decay: float = 1.0,
+        epsilon: float = 1.0,
     ) -> None:
         self.action_list = action_list
         self.learning_rate = learning_rate
         self.reward_decay = reward_decay
         self.epsilon = epsilon
-        self.q_table = pd.DataFrame(columns=action_list, dtype=np.float64)
+        self.q_table = {}
+        self.deprecated_state = []
         self.last_action = None
 
     def exist(self, state: str) -> bool:
-        """检测目前 Q 表中是否存在 state 状态"""
-        return state in self.q_table.index
+        return state in self.q_table.keys()
 
     def add_new_state(self, state: str):
-        """为 Q 表添加新状态"""
-        new_row = pd.DataFrame(
-            data=np.array([0] * len(self.action_list)).reshape(1, 4),
-            index=[state],
-            columns=self.action_list,
-            dtype=np.float64,
-        )
-        self.q_table = pd.concat([self.q_table, new_row])
+        self.q_table[state] = [0] * 4
 
-    def remove_action(self, action_list, action: int):
-        if action in action_list:
-            filtered_action_list = action_list.copy()
-            filtered_action_list.remove(action)
-            return filtered_action_list
+    def remove_action(self, action_reward_list, action: int):
+        """
+        Set it to -1 means never choose it again in this environment situation
+        """
+        action_reward_list[action] = -1
+
+    def filter_action(self, action_reward_list: List[int]) -> List[int]:
+        """
+        Give the action_reward_list, return an avaiable action set.
+        """
+        filtered_action = []
+        for action in range(len(action_reward_list)):
+            if action_reward_list[action] != -1:
+                filtered_action.append(action)
+        return filtered_action
 
     def reverse_action(self, action: int) -> int:
         """reverse action"""
@@ -53,43 +56,35 @@ class QLearning(Solver):
             raise ValueError("action is not valid")
 
     def make_decision(self, state: str) -> int:
-        """通过 Q 表，对于现在所处状态做决策，选出最大回报的方向"""
         if not self.exist(state):
             self.add_new_state(state)
-        if np.random.uniform() < self.epsilon:
-            action_reward_list = self.q_table.loc[state, :]
-            if self.last_action is not None:
-                filtered_action_list = self.remove_action(
-                    self.action_list, self.reverse_action(self.last_action)
-                )
-                action_reward_list = action_reward_list[filtered_action_list]
-            best_choices = action_reward_list[
-                action_reward_list == np.max(action_reward_list)
-            ].index.tolist()
-        else:
-            best_choices = self.action_list
-        # never look back
-        if self.last_action is not None:
-            self.remove_action(best_choices, self.reverse_action(self.last_action))
-        choice = np.random.choice(best_choices)
-
+        action_reward_list = self.q_table[state]
+        filtered_action = self.filter_action(action_reward_list)
+        choice = np.random.choice(filtered_action)
         self.last_action = choice
         return choice
 
     def learn(self, state: str, action: int, reward: int, next_state: str):
-        """走完一步就学习。
-        利用走完之后的 reward 进行学习"""
-        # It will not add state when it's dead
-        predict_value = self.q_table.loc[state, action]
-        if reward == 0:
-            if not self.exist(next_state):
-                self.add_new_state(next_state)
-            fixed_value = (
-                reward + self.reward_decay * self.q_table.loc[next_state, action]
+        if reward == -1 or next_state in self.deprecated_state:
+            """
+            reward == -1 means that the next_state is wall/out-bound
+            when agent choose to go random this time, it should also not
+            choose a direction like this.
+            """
+            self.q_table[state][action] += (
+                self.learning_rate
+                * self.reward_decay
+                * (-1 - self.q_table[state][action])
             )
-        else:
-            fixed_value = reward
-        # learn
-        self.q_table.loc[state, action] += self.learning_rate * (
-            fixed_value - predict_value
-        )
+            if np.max(self.q_table[state]) == -1:
+                self.deprecated_state.append(state)
+        if reward == 0:
+            """
+            reward == 0 means that the next_state is still a road
+            so the best value of four direction of next_state should also be zero
+            (Because we finish the process with reaching end only once,
+            there's almost all the road that best value of four dir is zero)
+            And because the reward of one direction is initialize with zero
+            we can literally jump this meaningless calculation
+            """
+            pass
